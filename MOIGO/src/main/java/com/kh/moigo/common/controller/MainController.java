592fbdae50a273c5bcd45a547c563dc4fb8d7015
이원제 <onejelee93@gmail.com>
2018-07-18 19:37:35 +0900

    footer page commit

--- MOIGO/src/main/java/com/kh/moigo/common/controller/MainController.java
+++ MOIGO/src/main/java/com/kh/moigo/common/controller/MainController.java
@@ -6,9 +6,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 
-	@RequestMapping("/common/main.do")
-	public String main(){
-		return "/main";
+	
+	@RequestMapping("/common/serviceInfo.ft")
+	public String serviceInfo(){
+		return "common/serviceInfo";
 	}
+	
+	@RequestMapping("/common/personalInfo.ft")
+	public String personalInfo(){
+		return "common/personalInfo";
+	}
+	
+	@RequestMapping("/common/useGuide.ft")
+	public String useGuide(){
+		return "common/useGuide";
+	}
+	
+	@RequestMapping("/common/maps.ft")
+	public String maps(){
+		return "common/maps";
+	}
+	
 	
 }
