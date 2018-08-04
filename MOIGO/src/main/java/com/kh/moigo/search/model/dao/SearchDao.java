3013f8e6f8588cb7ba4eeb388688fb137caaa1b4
lsh <lsh@LAPTOP-EF93JO21>
2018-08-04 14:02:19 +0900

    lsh update

--- MOIGO/src/main/java/com/kh/moigo/search/model/dao/SearchDao.java
+++ MOIGO/src/main/java/com/kh/moigo/search/model/dao/SearchDao.java
@@ -7,10 +7,6 @@ import com.kh.moigo.groups.model.vo.Groups;
 
 public interface SearchDao {
 
-	int listCount(String keyword);
-
-	List<Groups> selectList(String keyword, int cPage, int limit);
-
 	int detailListCount(Map<String, String> map);
 
 	List<Groups> detailSelectList(Map<String, String> map);
